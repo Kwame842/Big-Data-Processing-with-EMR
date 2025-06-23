b2eea586832c6af40ecc6f1ee595e5f880947b4c
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 22:32:38 +0000

    Job 2 packages import

---
+++ car-rental-pipeline/scripts/job2_user_metrics.py
@@ -0,0 +1,7 @@
+# job2_user_metrics.py
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import col, sum, count, avg, max, min, to_date, to_timestamp, unix_timestamp
+import sys
+import logging
