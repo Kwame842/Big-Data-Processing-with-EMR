fba2156e473254fd6c4dd1c278c55d3d422a498a
Kwame842 <kwame.boateng@amalitechtraining.org>
2025-06-23 22:33:21 +0000

    Job 2 logging setup

--- car-rental-pipeline/scripts/job2_user_metrics.py
+++ car-rental-pipeline/scripts/job2_user_metrics.py
@@ -5,3 +5,9 @@ from pyspark.sql.types import *
 from pyspark.sql.functions import col, sum, count, avg, max, min, to_date, to_timestamp, unix_timestamp
 import sys
 import logging
+
+# ----------------------------
+# Logging setup
+# ----------------------------
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("UserTransactionMetrics")
